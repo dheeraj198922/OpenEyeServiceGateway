d9ade2efb3d67d8d307593951495b914e5f67335
dheeraj chaudhary <dheerajneerajshiv@gmail.com>
2024-07-08 00:00:29 +0530

    dockerized changes

--- src/test/java/com/openeye/getway/ServiceGateWayApplicationTests.java
+++ src/test/java/com/openeye/getway/ServiceGateWayApplicationTests.java
@@ -1,13 +1,11 @@
 package com.openeye.getway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServiceGateWayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ServiceGateWayApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
